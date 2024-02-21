c07d73a560c31897da73efc4813091ff4f5c30e6
Nick <naookiokd@gmail.com>
2024-02-21 21:12:33 +0900

    review type

--- src/zod/product.ts
+++ src/zod/product.ts
@@ -1,18 +1,21 @@
-import { Primitive, z } from "zod"
+import { z } from "zod"
 import { toZodSchema } from "./common"
-import { ProductReview } from "@prisma/client"
+import { ProductReview, StripeCheckout } from "@prisma/client"
 
 export const zodProductRate = z.number().min(1).max(5)
 export const zodReview = z.string().min(1).max(500)
 export const zodReviewer = z.string().min(1).max(10)
+export const zodEmail = z.string().email()
 
-type ProductReviewCreationNecessaryField = "productId" | "reviewer" | "text" | "rating"
-type ProductReviewCreationParams = Pick<ProductReview, ProductReviewCreationNecessaryField> & { "serialNumber": string }
+type ProductReviewNecessaryField = "productId" | "reviewer" | "text" | "rating"
+type StripeCheckoutNecessaryField = "email" | "serialNumber"
+type ProductReviewCreationParams = Pick<ProductReview, ProductReviewNecessaryField> & Pick<StripeCheckout, StripeCheckoutNecessaryField>
 
 export const zodProductReviewCreationSchema = z.object<toZodSchema<ProductReviewCreationParams>>({
   productId: z.number(),
   reviewer: zodReviewer,
   text: zodReview,
   rating: zodProductRate,
-  serialNumber: z.string()
+  serialNumber: z.string(),
+  email: zodEmail
 }) 
