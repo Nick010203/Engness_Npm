ed93555b227f81f7af090f97a1345bdff8f9bbf2
Nick <naookiokd@gmail.com>
2024-02-22 20:11:58 +0900

    \nがそのまま描画されてしまったので変換しない

--- src/function/sanitizeHtml.ts
+++ src/function/sanitizeHtml.ts
@@ -2,10 +2,6 @@ import sanitizeHtml from 'sanitize-html';
 
 export const sanitizeHtmlString = (html: string) => {
   const cleanHtml = sanitizeHtml(html)
-  if (cleanHtml) {
-    // 改行は \n に変換する
-    return cleanHtml.replace(/\n/g, '\\n')
-  }
-
-  return ""
+  // サニタイズした結果が無効なら空の文字列になる
+  return cleanHtml
 }
