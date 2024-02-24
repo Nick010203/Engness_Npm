0d25e029ffe3b4176fac4ff96893fb974dd71c23
Nick <naookiokd@gmail.com>
2024-02-24 13:40:49 +0900

    userからpassword廃止

--- src/service/user/userUpdateParams.ts
+++ src/service/user/userUpdateParams.ts
@@ -3,10 +3,8 @@ import { zodEmail, zodUserBasicInfoUpdateSchema } from "../../zod/user"
 
 export type userBasicInfoUpdateParams = z.infer<typeof zodUserBasicInfoUpdateSchema>
 export type userEmailUpdateParams = { email: z.infer<typeof zodEmail> }
-export type userPasswordUpdateParams = { oldPassword: string, newPassword: string }
-
 
 export type userUpdateParams = {
-  updateType: "BASIC" | "EMAIL" | "PASSWORD"
-  updateData: userBasicInfoUpdateParams | userEmailUpdateParams | userPasswordUpdateParams
+  updateType: "BASIC" | "EMAIL"
+  updateData: userBasicInfoUpdateParams | userEmailUpdateParams
 }
--- src/zod/user.ts
+++ src/zod/user.ts
@@ -3,10 +3,11 @@ import { User, Gender } from "@prisma/client"
 import { toZodSchema } from "./common"
 
 const userNameRegex = /^[a-zA-Z0-9]+$/
+// password使わなくなったが一応残しておく
 const passwordRegex = /^[a-zA-Z0-9-_]+$/
 
-type UserCreationNecessaryField = "userName" | "email" | "gender" | "birthYear" | "city" | "password"
-type UserLoginNecessaryField = "email" | "password"
+type UserCreationNecessaryField = "userName" | "email" | "gender" | "birthYear" | "city"
+type UserLoginNecessaryField = "email"
 
 type UserCreationParams = Pick<User, UserCreationNecessaryField>
 type EmailLoginParams = Pick<User, UserLoginNecessaryField>
@@ -17,6 +18,7 @@ export const zodGender = z.enum(["male", "female", "middle"])
 export const zodEmail = z.string().email()
 export const zodBirthday = z.string()
 export const zodCity = z.string()
+// password使わないようになったがregexの使い方の参考として一応残しておく
 export const zodPassword = z.string().min(8, { message: "More than 8 words" }).max(32, { message: "Less than 32 words" }).regex(passwordRegex, { message: "0-9 or a-z or A-Z" })
 
 export const zodUserCreationSchema = z.object<toZodSchema<UserCreationParams>>({
@@ -25,15 +27,13 @@ export const zodUserCreationSchema = z.object<toZodSchema<UserCreationParams>>({
   email: zodEmail,
   birthYear: zodBirthday,
   city: zodCity,
-  password: zodPassword
 })
 
 export const zodEmailLoginSchema = z.object<toZodSchema<EmailLoginParams>>({
   email: zodEmail,
-  password: zodPassword
 })
 
 
-export const zodUserBasicInfoUpdateSchema = zodUserCreationSchema.omit({ email: true, password: true })
+export const zodUserBasicInfoUpdateSchema = zodUserCreationSchema.omit({ email: true })
 
 
