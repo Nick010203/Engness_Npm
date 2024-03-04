59073875b1cb36948c5d6662962f2af2d6913138
Nick <naookiokd@gmail.com>
2024-03-04 20:25:43 +0900

    create admin user zod

--- src/zod/admin.ts
+++ src/zod/admin.ts
@@ -1,12 +1,15 @@
 import z from "zod"
-import { AdminUser } from "@prisma/client"
-import { toZodSchema, zodEmail, zodPassword } from "./common"
+import { AdminUser, AdminRole } from "@prisma/client"
+import { toZodSchema, zodEmail, zodPassword, zodUserName } from "./common"
 
-type AdminUserLoginNecessaryField = "email" | "password"
-
+type AdminUserLoginNecessaryField = "userName" | "role" | "email" | "password"
 type AdminUserLoginParams = Pick<AdminUser, AdminUserLoginNecessaryField>
 
+const adminRoleSchema = z.nativeEnum(AdminRole)
+
 export const zodAdminUserLoginSchema = z.object<toZodSchema<AdminUserLoginParams>>({
+  role: adminRoleSchema,
+  userName: zodUserName,
   email: zodEmail,
   password: zodPassword
 })
