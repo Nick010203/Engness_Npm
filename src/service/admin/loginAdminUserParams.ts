47a3b6ffc6630b54a32973ff7fb608b34572acd2
Nick <naookiokd@gmail.com>
2024-03-05 21:04:53 +0900

    zod admin login

--- src/service/admin/loginAdminUserParams.ts
+++ src/service/admin/loginAdminUserParams.ts
@@ -2,3 +2,7 @@ import { z } from "zod"
 import { zodAdminUserLoginSchema } from "../../zod/admin"
 
 export type loginAdminUserParams = z.infer<typeof zodAdminUserLoginSchema>
+
+export type loginAdminUserResponse = {
+  jwt: string
+}
