6aad107a58277827adbdedd1900757abc8699ea2
Nick <naookiokd@gmail.com>
2024-03-22 19:21:47 +0900

    next #71

--- src/service/admin/adminUserDataForFront.ts
+++ src/service/admin/adminUserDataForFront.ts
@@ -3,4 +3,4 @@ import { AdminUser } from "@prisma/client"
 const adminUserColumnForFront = ["id", "role", "adminName", "adminEmail"] as const
 type adminUserColumnForFront = (typeof adminUserColumnForFront)[number]
 
-export type adminUserDataForFront = Pick<AdminUser, adminUserColumnForFront>
+export type adminUserDataForFront = Pick<AdminUser, adminUserColumnForFront> & { accessToken: string }
--- src/service/user/userDataForFront.ts
+++ src/service/user/userDataForFront.ts
@@ -3,4 +3,4 @@ import { User } from "@prisma/client"
 const userColumnForFront = ['id', 'userName', 'email', 'gender', 'city', 'birthYear'] as const
 type userColumnForFront = (typeof userColumnForFront)[number]
 
-export type userDataForFront = Pick<User, userColumnForFront> 
+export type userDataForFront = Pick<User, userColumnForFront> & { accessToken: string }
