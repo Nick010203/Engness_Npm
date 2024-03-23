1fa124fd6641d97183df64768ed8ab8e13c6c2e5
Nick <naookiokd@gmail.com>
2024-03-23 19:54:53 +0900

    next #71

--- src/service/user/login/emailLoginParams.ts
+++ src/service/user/login/emailLoginParams.ts
@@ -1,4 +1,8 @@
 import { z } from "zod"
 import { zodEmailLoginSchema } from "../../../zod/user"
+import { userDataForFront } from "../userDataForFront"
+
+export type emailLoginPrepareParams = z.infer<typeof zodEmailLoginSchema>
 
 export type emailLoginParams = z.infer<typeof zodEmailLoginSchema>
+export type emailLoginResponse = userDataForFront
--- src/service/user/login/emailLoginValidationParams.ts
+++ src/service/user/login/emailLoginValidationParams.ts
@@ -1,7 +1,6 @@
-import { userDataForFront } from "../userDataForFront"
 export type emailLoginValidationParams = {
   email: string
   oneTimePassword: string
 }
 
-export type emailLoginValidationResponse = { userData: userDataForFront, isExpiredOneTimePassword: boolean }
+export type emailLoginValidationResponse = { isExpiredOneTimePassword: boolean }
