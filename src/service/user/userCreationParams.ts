9bcfa2d88ef0fb4bc6591832a2690d33f798e7d1
Nick <naookiokd@gmail.com>
2024-03-23 20:56:55 +0900

    next #71

--- src/service/user/userCreationParams.ts
+++ src/service/user/userCreationParams.ts
@@ -1,4 +1,7 @@
 import { z } from "zod"
 import { zodUserCreationSchema } from "../../zod/user"
+import { userDataForFront } from "./userDataForFront"
 
 export type userCreationParams = z.infer<typeof zodUserCreationSchema>
+
+export type userActivationParams = userDataForFront
