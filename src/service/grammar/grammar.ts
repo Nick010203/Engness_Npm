630b4ffa2153ade00569a096ff601257389b6fba
Nick <naookiokd@gmail.com>
2024-04-11 21:00:59 +0900

    node #58

--- src/service/grammar/grammar.ts
+++ src/service/grammar/grammar.ts
@@ -1,15 +1,17 @@
 import { Grade, Grammar, GrammarQuestion } from "@prisma/client"
 
+export type GrammarQuestionType = Omit<GrammarQuestion, "question"> & { "question": Record<"q1" | "q2" | "q3" | "q4" | "q5" | "q6", string>[] }
+
 export type gradeWithGrammarList = Grade & {
   Grammar: Grammar[]
 }
 
 export type grammarWithGradeAndGrammarQuestion = Grammar & {
   Grade: Grade,
-  GrammarQuestion: Partial<GrammarQuestion>[]
+  GrammarQuestion: Partial<GrammarQuestionType>[]
 }
 
-export type grammarQuestionWithGradeAndGrammar = Partial<GrammarQuestion> & {
+export type grammarQuestionWithGradeAndGrammar = Partial<GrammarQuestionType> & {
   Grammar: Partial<Grammar> & {
     Grade: Grade,
   }
