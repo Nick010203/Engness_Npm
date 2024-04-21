c7fedcf2e4a341b605d205b861c09fa7a20e5e19
Nick <naookiokd@gmail.com>
2024-04-22 00:07:27 +0900

    next #95

--- src/service/listening/listeningType.ts
+++ src/service/listening/listeningType.ts
@@ -3,5 +3,5 @@ import { Listening, LikeListening } from '@prisma/client'
 export type sortOption = "all" | "sortByLap" | "unLikedOnly" | "likedOnly"
 
 export type ListeningWithLike = Listening & {
-  LikeListening: LikeListening
+  LikeListening?: LikeListening
 }
