4cdc750026d7e24123f67830649d3c7b65938ac2
Nick <naookiokd@gmail.com>
2024-04-27 12:53:04 +0900

    node #64

--- src/service/grammar/grammar.ts
+++ src/service/grammar/grammar.ts
@@ -8,10 +8,12 @@ export type gradeWithGrammarList = Grade & {
 
 export type grammarWithGradeAndGrammarQuestion = Grammar & {
   Grade: Grade,
-  GrammarQuestion: Partial<TypeAssertedGrammarQuestion>[]
+  GrammarQuestion: Partial<TypeAssertedGrammarQuestion>[] & {
+    LikeGrammarQuestion?: LikeGrammarQuestion[]
+  }
 }
 
-export type grammarQuestionWithGradeAndGrammarAndLike = Partial<TypeAssertedGrammarQuestion> & {
+export type grammarQuestionWithGradeAndGrammar = Partial<TypeAssertedGrammarQuestion> & {
   Grammar: Partial<Grammar> & {
     Grade: Grade,
   } & {
