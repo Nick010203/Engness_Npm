97a446b800c5adcf4ac6ab11f3ad5ca06575970a
Nick <naookiokd@gmail.com>
2024-04-27 13:12:51 +0900

    node #64

--- src/service/grammar/grammar.ts
+++ src/service/grammar/grammar.ts
@@ -1,6 +1,6 @@
 import { Grade, Grammar, GrammarQuestion, LikeGrammarQuestion } from "@prisma/client"
 
-export type TypeAssertedGrammarQuestion = Omit<GrammarQuestion, "question"> & { "question": Record<"q1" | "q2" | "q3" | "q4" | "q5" | "q6", string>[] } & { LikeGrammarQuestion: LikeGrammarQuestion[] }
+export type TypeAssertedGrammarQuestion = Omit<GrammarQuestion, "question"> & { "question": Record<"q1" | "q2" | "q3" | "q4" | "q5" | "q6", string>[] | undefined } & { LikeGrammarQuestion: LikeGrammarQuestion[] }
 
 export type gradeWithGrammarList = Grade & {
   Grammar: Grammar[]
