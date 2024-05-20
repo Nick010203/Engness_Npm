662f9beca993ec3f68055068b0b8fca18d149281
Nick <naookiokd@gmail.com>
2024-05-20 23:40:17 +0900

    node #26

--- src/service/user/userDataForFront.ts
+++ src/service/user/userDataForFront.ts
@@ -2,5 +2,8 @@ import type { User, subscriptionPlan } from "@prisma/client"
 
 export const userColumnForFront = ['id', 'userName', 'email', 'gender', 'city', 'birthYear'] as const
 type userColumnForFront = (typeof userColumnForFront)[number]
-
 export type userDataForFront = Pick<User, userColumnForFront> & { subscribingPlan: subscriptionPlan }
+
+export const sessionColumnForFront = ["id", 'userName'] as const
+type sessionColumnForFront = (typeof sessionColumnForFront)[number]
+export type sessionDataForFront = Pick<User, sessionColumnForFront> & { accessToken: string }
