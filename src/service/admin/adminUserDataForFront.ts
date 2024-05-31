c9b00515c23f58c663cdcb990e5bbec99b90b9c4
Nick <naookiokd@gmail.com>
2024-05-31 20:41:42 +0900

    Next #115

--- src/service/admin/adminUserDataForFront.ts
+++ src/service/admin/adminUserDataForFront.ts
@@ -1,9 +1,9 @@
 import { AdminUser } from "@prisma/client"
 
-const adminUserColumnForFront: Array<keyof AdminUser> = ["id", "role", "adminName", "adminEmail"] as const
+const adminUserColumnForFront = ["id", "role", "adminName", "adminEmail"] as const
 type adminUserColumnForFront = (typeof adminUserColumnForFront)[number]
 export type adminUserDataForFront = Pick<AdminUser, adminUserColumnForFront>
 
-export const adminSessionColumnForFront: Array<keyof AdminUser> = ["id", 'adminName', "role"] as const
+export const adminSessionColumnForFront = ["id", 'adminName', "role",] as const
 type adminSessionColumnForFront = (typeof adminSessionColumnForFront)[number]
 export type adminSessionDataForFront = Pick<AdminUser, adminSessionColumnForFront> & { accessToken: string, isAdmin: true }
