a14c02704115590e5e8d00c5cad530c74e56d7e5
Nick <naookiokd@gmail.com>
2024-05-31 20:30:14 +0900

    Next #115

--- src/service/admin/loginAdminUserParams.ts
+++ src/service/admin/loginAdminUserParams.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 import { zodAdminUserLoginSchema } from "../../zod/admin"
-import { adminUserDataForFront } from "./adminUserDataForFront"
+import { adminSessionDataForFront } from "./adminUserDataForFront"
 
 export type loginAdminUserParams = z.infer<typeof zodAdminUserLoginSchema>
 
-export type loginAdminUserResponse = adminUserDataForFront
+export type loginAdminUserResponse = adminSessionDataForFront
