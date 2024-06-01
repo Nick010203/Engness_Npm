016f617ac68fc12300a422234ca881e2baa3e51a
Nick <naookiokd@gmail.com>
2024-06-01 17:26:02 +0900

    Next #114

--- src/service/admin/adminUserDataForFront.ts
+++ src/service/admin/adminUserDataForFront.ts
@@ -1,9 +1,9 @@
 import { AdminUser } from "@prisma/client"
 
 const adminUserColumnForFront = ["id", "role", "adminName", "adminEmail"] as const
-type adminUserColumnForFront = (typeof adminUserColumnForFront)[number]
+export type adminUserColumnForFront = (typeof adminUserColumnForFront)[number]
 export type adminUserDataForFront = Pick<AdminUser, adminUserColumnForFront>
 
 export const adminSessionColumnForFront = ["id", 'adminName', "role",] as const
-type adminSessionColumnForFront = (typeof adminSessionColumnForFront)[number]
+export type adminSessionColumnForFront = (typeof adminSessionColumnForFront)[number]
 export type adminSessionDataForFront = Pick<AdminUser, adminSessionColumnForFront> & { accessToken: string, isAdmin: true }
--- src/service/user/userDataForFront.ts
+++ src/service/user/userDataForFront.ts
@@ -2,9 +2,9 @@ import type { User } from "@prisma/client"
 import type { availablePlanList } from "../stripe/subscriptionPlanAndPriceIdMap"
 
 export const userColumnForFront = ['id', 'userName', 'email', 'gender', 'city', 'birthYear'] as const
-type userColumnForFront = (typeof userColumnForFront)[number]
+export type userColumnForFront = (typeof userColumnForFront)[number]
 export type userDataForFront = Pick<User, userColumnForFront> & { subscribingPlan: availablePlanList }
 
 export const sessionColumnForFront = ["id", 'userName'] as const
-type sessionColumnForFront = (typeof sessionColumnForFront)[number]
+export type sessionColumnForFront = (typeof sessionColumnForFront)[number]
 export type sessionDataForFront = Pick<User, sessionColumnForFront> & { accessToken: string }
