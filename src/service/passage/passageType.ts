562be1bbe21aa004248adc8236b2254e16153575
Nick <naookiokd@gmail.com>
2024-06-08 15:41:59 +0900

    Next #109

--- src/service/passage/passageType.ts
+++ src/service/passage/passageType.ts
@@ -11,28 +11,28 @@ export type overWrittenSinglePassageQuestion = {
   answer: Array<string | number> // 複数回答もありえるので
 }
 
-export const passageLevelMap: { level: PassageLevel, list: string }[] = [
+export const passageLevelMap: { level: PassageLevel, examLevel: string }[] = [
   {
     level: "C2",
-    list: "IELTS 9.0 ~ 8.5"
+    examLevel: "IELTS 9.0 ~ 8.5"
   },
   {
     level: "C1",
-    list: "英検 1, IELTS 8.0 ~ 7.0"
+    examLevel: "英検 1, IELTS 8.0 ~ 7.0"
   }, {
     level: "B2",
-    list: "英検 1 ~ Pre1, IELTS 6.5 ~ 5.5"
+    examLevel: "英検 1 ~ Pre1, IELTS 6.5 ~ 5.5"
   },
   {
     level: "B1",
-    list: "英検 Pre1 ~ 2, IELTS 5.0 ~ 4.0"
+    examLevel: "英検 Pre1 ~ 2, IELTS 5.0 ~ 4.0"
   },
   {
     level: "A2",
-    list: "英検 2 ~ Pre2"
+    examLevel: "英検 2 ~ Pre2"
   },
   {
     level: "A1",
-    list: "英検 Pre2 ~ 3"
+    examLevel: "英検 Pre2 ~ 3"
   }
 ]
