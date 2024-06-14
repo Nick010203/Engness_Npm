426c25fccdaa239013134a138419b867254335c0
Nick <naookiokd@gmail.com>
2024-06-14 19:03:52 +0900

    Next #96

--- src/service/seo/getMetaTagParams.ts
+++ src/service/seo/getMetaTagParams.ts
@@ -2,7 +2,19 @@ type grammarSeo = "grammarList" | "singleGrammar"
 type listeningSeo = "singleListening"
 
 
-export type getMetaTagParams = grammarSeo | listeningSeo
+export type getMetaTagPurpose = grammarSeo | listeningSeo
+
+// 条件型を使用して `T` に基づいて `P` の型を決定
+type Params<T> =
+  T extends "grammarList" ? { gradeId: number } :
+  T extends "singleGrammar" ? { grammarId: number } :
+  T extends "singleListening" ? { listeningId: number } :
+  never;
+
+export type getMetaTagParams<T extends getMetaTagPurpose> = {
+  purpose: T,
+  params: Params<T>
+};
 
 export type getMetaTagResponse = {
   title: string
