a13792f640ef3a7806afe82671d2491623c4ded1
Nick <naookiokd@gmail.com>
2024-06-15 18:32:23 +0900

    Next #54

--- src/service/blog/blogType.ts
+++ src/service/blog/blogType.ts
@@ -1,6 +1,8 @@
 import type { Blog, BlogCategory } from "@prisma/client"
 
-export type blogCategory = "英検" | "大学受験" | "高校受験" | "留学" | "各種紹介" | "共通テスト" | "本社"
+export const blogCategoryList = ["英検", "大学受験", "高校受験", "留学", "各種紹介", "共通テスト", "本社"] as const
+
+export type blogCategory = (typeof blogCategoryList)[number]
 
 export type blogListGroupedByCategory = {
   [K in blogCategory]: Blog[]
