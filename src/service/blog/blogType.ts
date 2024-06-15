6e080a62f99df06dd5b8c955eacdf0dae96fd8ec
Nick <naookiokd@gmail.com>
2024-06-15 17:32:53 +0900

    Next #54

---
+++ src/service/blog/blogType.ts
@@ -0,0 +1,9 @@
+import type { Blog, BlogCategory } from "@prisma/client"
+
+type blogCategory = "英検" | "大学受験" | "高校受験" | "留学" | "各種紹介" | "共通テスト" | "本社"
+
+export type blogListGroupedByCategory = {
+  [K in blogCategory]: {
+    blog: Blog
+  }
+}
