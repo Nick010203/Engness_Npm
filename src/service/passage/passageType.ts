49818f3771893ff4172be0b995ad1707d80979e1
Nick <naookiokd@gmail.com>
2024-07-11 23:45:54 +0900

    Node #95

--- src/service/passage/passageType.ts
+++ src/service/passage/passageType.ts
@@ -1,4 +1,4 @@
-import { Passage, PassageQuestion, PassageSentence } from "@prisma/client"
+import { Passage, PassageQuestion, PassageSentence, EnglishLevel } from "@prisma/client"
 
 export type PassageWithRelatedData = Passage & {
   PassageQuestion: PassageQuestion | null,
@@ -28,29 +28,3 @@ export type SingleQuestion = {
 export type SinglePassageQuestion = Omit<PassageQuestion, "questionList"> & {
   questionList: SingleQuestion[]
 }
-
-export const passageLevelMap: { level: string, examLevel: string }[] = [
-  {
-    level: "C2",
-    examLevel: "IELTS 9.0 ~ 8.5"
-  },
-  {
-    level: "C1",
-    examLevel: "英検 1, IELTS 8.0 ~ 7.0"
-  }, {
-    level: "B2",
-    examLevel: "英検 1 ~ Pre1, IELTS 6.5 ~ 5.5"
-  },
-  {
-    level: "B1",
-    examLevel: "英検 Pre1 ~ 2, IELTS 5.0 ~ 4.0"
-  },
-  {
-    level: "A2",
-    examLevel: "英検 2 ~ Pre2"
-  },
-  {
-    level: "A1",
-    examLevel: "英検 Pre2 ~ 3"
-  }
-]
--- src/zod/passage.ts
+++ src/zod/passage.ts
@@ -1,16 +1,12 @@
 import { ZodAnyDef, z } from "zod"
 import { toZodSchema } from "./common";
-import { PassageLevel, Passage } from "@prisma/client";
+import { Passage, EnglishLevel, EnglishLevelRange } from "@prisma/client";
 import { SingleQuestion } from "../service/passage/passageType"
 
-const passageLevel: PassageLevel[] = ["C2", "C1", "B2", "B1", "A2", "A1"] as const
-// これなに... むず... でも上手く動いてそう
-const passageLevelEnum = z.enum([...passageLevel] as [PassageLevel, ...PassageLevel[]])
-
-type PartialPassage = Pick<Passage, "level" | "title">
+type PartialPassage = Pick<Passage, "englishLevelId" | "title">
 
 export const zodPassageCreationSchema = z.object<toZodSchema<PartialPassage>>({
-  level: passageLevelEnum,
+  englishLevelId: z.number(),
   title: z.string().min(5)
 })
 
