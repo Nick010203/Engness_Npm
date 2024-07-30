02c8a311c09c2d367a53749176871783ed9f76be
Nick <naookiokd@gmail.com>
2024-07-30 21:03:52 +0900

    Node #105

--- src/service/email/emailSendingParams.ts
+++ src/service/email/emailSendingParams.ts
@@ -5,7 +5,7 @@ export type mailType = 'stripeCheckoutComplete' | 'userSignUp' | 'userSignIn' | 
 export type mailFrom = "no_reply@engness.com" | "hello@engness.com"
 
 // backend to API Gateway
-export type emailSendingParamsFromBackend = {
+export type emailSendingParams = {
   mailType: mailType
   mailFrom: mailFrom
   mailSubject: string
