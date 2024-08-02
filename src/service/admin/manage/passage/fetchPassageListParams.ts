f783659ffd42051f2508c35afbc8fdacc8ec688d
Nick <naookiokd@gmail.com>
2024-08-03 01:01:25 +0900

    Node #74

--- src/service/admin/manage/passage/fetchPassageListParams.ts
+++ src/service/admin/manage/passage/fetchPassageListParams.ts
@@ -1,4 +1,4 @@
 
 import { Passage, EnglishLevel } from "@prisma/client"
 
-export type fetchPassageListResponse = Omit<Passage, "englishLevelId"> & { "EnglishLevel": EnglishLevel }[]
+export type fetchPassageListResponse = Omit<Passage, "englishLevelId"> & { "EnglishLevel": EnglishLevel | null }[]
