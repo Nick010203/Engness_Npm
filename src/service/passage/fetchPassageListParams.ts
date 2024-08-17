ceb2d4b5c7bf804563ac56b505bc6768b25e95e7
Nick <naookiokd@gmail.com>
2024-08-17 12:03:07 +0900

    Node #125

--- src/service/passage/fetchPassageListParams.ts
+++ src/service/passage/fetchPassageListParams.ts
@@ -3,4 +3,7 @@ import type { Passage, EnglishLevel } from "@prisma/client"
 export type fetchPassageListParams = {
   englishLevelId: string | "all"
 }
-export type fetchPassageListResponse = { englishLevel: EnglishLevel, passageList: Passage & { challengeable: boolean }[] }
+export type fetchPassageListResponse = {
+  englishLevel: EnglishLevel,
+  passageList: { [key: string]: Passage & { challengeable: boolean }[] }
+}
