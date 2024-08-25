a5b2acd05b33873769db92373dd5caa83bfc89c1
Nick <naookiokd@gmail.com>
2024-08-25 10:19:22 +0900

    Node #125

---
+++ src/service/passage/fetchSinglePassageDataParams.ts
@@ -0,0 +1,7 @@
+import { PassageWithRelatedData } from "../passage/passageType"
+
+export type fetchSinglePassageParams = {
+  passageId: number
+}
+
+export type fetchSinglePassageDataResponse = PassageWithRelatedData
