f3c6b102f0ce626aa8ba5479e50bc0fabaa9fea3
Nick <naookiokd@gmail.com>
2024-09-06 19:41:40 +0900

    Node #130

--- src/service/passage/fetchPassageUpdateInformationParams.ts
+++ src/service/passage/fetchPassageUpdateInformationParams.ts
@@ -1,9 +1,5 @@
 import type { PassageUpdateInformation } from "@prisma/client"
 
-export type fetchPassageUpdateInformationParams = {
-  passageId: number
-}
-
 export type fetchPassageUpdateInformationResponse = {
   accessAble: boolean
   passageUpdateInformation?: PassageUpdateInformation[]
