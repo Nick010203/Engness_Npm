4dbe0224d78ff3f7953c24a42d12a469f57f74d0
Nick <naookiokd@gmail.com>
2024-09-11 01:32:35 +0900

    Next #212

--- src/service/passage/splitArrayToString.ts
+++ src/service/passage/splitArrayToString.ts
@@ -1,6 +1,6 @@
 
 // 配列から , で区切られた文字列を作る
-export const splitArrayToString = (arrOrStr: Array<string> | string) => {
+export const splitArrayToString = (arrOrStr: Array<string | number> | string) => {
   return Array.isArray(arrOrStr)
     ? arrOrStr.join(', ')
     : arrOrStr ?? ''
