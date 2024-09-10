cafdcb24577cac98b84d60950fa2d1a53c3eecd4
Nick <naookiokd@gmail.com>
2024-09-11 01:24:51 +0900

    Next #212

--- src/service/passage/splitArrayToString.ts
+++ src/service/passage/splitArrayToString.ts
@@ -1,6 +1,6 @@
 
 // 配列から , で区切られた文字列を作る
-const splitArrayToString = (arrOrStr: Array<string> | string) => {
+export const splitArrayToString = (arrOrStr: Array<string> | string) => {
   return Array.isArray(arrOrStr)
     ? arrOrStr.join(', ')
     : arrOrStr ?? ''
--- src/service/passage/splitStringToArray.ts
+++ src/service/passage/splitStringToArray.ts
@@ -1,5 +1,5 @@
 
 // "apple, banana ,  cherry" のような , で区切られた文字列を配列に変換
-const splitStringToArray = (str: string) => {
+export const splitStringToArray = (str: string) => {
   return str ? str.split(',').map((item) => item.trim()) : []
 }
