ee332fff4bbc71c45cd5a360e2b4fc491374b529
Nick <naookiokd@gmail.com>
2024-09-20 20:12:52 +0900

    Node #147

--- src/service/user/userUpdateParams.ts
+++ src/service/user/userUpdateParams.ts
@@ -5,7 +5,7 @@ import { zodEmail } from "../../zod/common"
 export type userBasicInfoUpdateParams = z.infer<typeof zodUserBasicInfoUpdateSchema>
 export type userEmailUpdateParams = { oldEmail: z.infer<typeof zodEmail>, newEmail: z.infer<typeof zodEmail> }
 
-export type userUpdateParams = {
-  updateType: "BASIC" | "EMAIL"
-  updateData: userBasicInfoUpdateParams | userEmailUpdateParams
+export type userUpdateParams<T extends "BASIC" | "EMAIL"> = {
+  updateType: T
+  updateData: T extends "BASIC" ? userBasicInfoUpdateParams : userEmailUpdateParams
 }
