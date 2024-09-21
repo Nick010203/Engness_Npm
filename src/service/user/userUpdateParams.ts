b19a5153d06c616625c956dae30b491745fe1a87
Nick <naookiokd@gmail.com>
2024-09-21 12:05:00 +0900

    Node #147

--- src/service/user/userUpdateParams.ts
+++ src/service/user/userUpdateParams.ts
@@ -5,7 +5,21 @@ import { zodEmail } from "../../zod/common"
 export type userBasicInfoUpdateParams = z.infer<typeof zodUserBasicInfoUpdateSchema>
 export type userEmailUpdateParams = { oldEmail: z.infer<typeof zodEmail>, newEmail: z.infer<typeof zodEmail> }
 
+type updateType = "BASIC" | "EMAIL"
+
 export type userUpdateParams = {
-  updateType: "BASIC" | "EMAIL"
-  updateData: userBasicInfoUpdateParams | userEmailUpdateParams
+  updateType: updateType;
+  updateData: userBasicInfoUpdateParams | userEmailUpdateParams;
+}
+
+export type userUpdateResponse<T extends updateType> = T extends "BASIC" ? null : { newEmail: string, oldEmail: string }
+
+export type userEmailUpdateOneTimePasswordValidationParams = {
+  newEmail: string
+  oldEmail: string
+  oneTimePassword: string
+}
+
+export type userEmailUpdateOneTimePasswordValidationResponse = {
+  isOneTimePasswordValidated: boolean
 }
