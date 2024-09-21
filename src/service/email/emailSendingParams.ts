d63888c111c0c28d39bfa66542b823bdc00c0eab
Nick <naookiokd@gmail.com>
2024-09-21 14:41:30 +0900

    Node #147

--- src/service/email/emailSendingParams.ts
+++
@@ -1,6 +0,0 @@
-// バックエンドでしか使ってないのにnpmにある意味ないかも...
-
-export type mailType = 'stripeCheckoutComplete' | 'userSignUp' | 'userSignIn' | 'adminUserSignIn' | "userEmailUpdate"
-
-// EmailAddress we use
-export type mailFrom = "no_reply@engness.com" | "hello@engness.com"
