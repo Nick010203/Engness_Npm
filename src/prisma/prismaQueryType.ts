8b96c28abf5e25de46bd8a0b0e74372fc3841c29
Nick <naookiokd@gmail.com>
2024-09-24 21:04:22 +0900

    Node #149

--- src/prisma/prismaQueryType.ts
+++ src/prisma/prismaQueryType.ts
@@ -1,4 +1,6 @@
-// 参考: https://github.com/prisma/prisma/issues/6980#:~:text=WhereInput%3C%27User%27%3E.-,Alternatives,-There%20is%20quite
+// 参考: https://github.com/prisma/prisma/issues/6980#issue-878506022
+
+// createとかのモデル操作のメソッドをやるなら参考: https://github.com/prisma/prisma/discussions/3929#discussioncomment-6635241
 
 import * as p from '@prisma/client'
 
@@ -10,13 +12,26 @@ type WhereInput<T> = T extends Model<Record<string, unknown>, p.Prisma.ModelName
   ? Exclude<Parameters<ModelDelegates[T['kind']]['findFirst']>[0], undefined | null>['where']
   : never
 
+type SelectInput<T> = T extends Model<Record<string, unknown>, p.Prisma.ModelName>
+  ? Exclude<Parameters<ModelDelegates[T['kind']]['findFirst']>[0], undefined | null>['select']
+  : never
+
+type IncludeInput<T> = T extends Model<Record<string, unknown>, p.Prisma.ModelName>
+  ? Exclude<Parameters<ModelDelegates[T['kind']]['findFirst']>[0], undefined | null>
+  : never
+
+
 type Model<T extends Record<string, unknown>, TName extends string> = T & { kind: TName }
 
 // ジェネリックなwhere型を定義
 export type WhereQuery<T extends keyof ModelMap> = WhereInput<ModelMap[T]>
 
 // ジェネリックなselect型を定義
-export type SelectQuery<T extends keyof ModelMap> = Partial<Record<keyof ModelMap[T], boolean>>
+export type SelectQuery<T extends keyof ModelMap> = SelectInput<ModelMap[T]>
+
+// ジェネリックなinclude型を定義 (たまたま成功した...)
+export type includeQuery<T extends keyof ModelMap> = Exclude<IncludeInput<ModelMap[T]>["include"], undefined | null>
+
 
 // limit型を定義
 export type LimitQuery = number
@@ -27,11 +42,13 @@ export type PrismaQueryRequest<T extends keyof ModelMap> = {
     where?: WhereQuery<T>
     select?: SelectQuery<T> // 指定なければ全フィールド
     limit?: LimitQuery
+    include?: includeQuery<T>
   }
 }
+
 
 // ここに追加していく
 export type ModelMap = {
   user: Model<p.User, 'User'>
-  adminUser: Model<p.User, 'AdminUser'>
+  adminUser: Model<p.AdminUser, 'AdminUser'>
 }
