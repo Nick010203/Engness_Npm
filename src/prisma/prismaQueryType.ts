f28505744d6ab2a9c2258ba6dc0137933a478ed8
Nick <naookiokd@gmail.com>
2024-10-06 15:20:57 +0900

    Next #246

--- src/prisma/prismaQueryType.ts
+++ src/prisma/prismaQueryType.ts
@@ -2,21 +2,21 @@
 
 // createとかのモデル操作のメソッドをやるなら参考: https://github.com/prisma/prisma/discussions/3929#discussioncomment-6635241
 
-import * as p from '@prisma/client'
+import * as prisma from '@prisma/client'
 
 type ModelDelegates = {
-  [K in p.Prisma.ModelName]: p.PrismaClient[Uncapitalize<K>]
+  [K in prisma.Prisma.ModelName]: prisma.PrismaClient[Uncapitalize<K>]
 }
 
-type WhereInput<T> = T extends Model<Record<string, unknown>, p.Prisma.ModelName>
+type WhereInput<T> = T extends Model<Record<string, unknown>, prisma.Prisma.ModelName>
   ? Exclude<Parameters<ModelDelegates[T['kind']]['findFirst']>[0], undefined | null>['where']
   : never
 
-type SelectInput<T> = T extends Model<Record<string, unknown>, p.Prisma.ModelName>
+type SelectInput<T> = T extends Model<Record<string, unknown>, prisma.Prisma.ModelName>
   ? Exclude<Parameters<ModelDelegates[T['kind']]['findFirst']>[0], undefined | null>['select']
   : never
 
-type IncludeInput<T> = T extends Model<Record<string, unknown>, p.Prisma.ModelName>
+type IncludeInput<T> = T extends Model<Record<string, unknown>, prisma.Prisma.ModelName>
   ? Exclude<Parameters<ModelDelegates[T['kind']]['findFirst']>[0], undefined | null>
   : never
 
@@ -32,10 +32,10 @@ export type SelectQuery<T extends keyof ModelMap> = SelectInput<ModelMap[T]>
 // ジェネリックなinclude型を定義 (たまたま成功した...)
 export type includeQuery<T extends keyof ModelMap> = Exclude<IncludeInput<ModelMap[T]>["include"], undefined | null>
 
-
 // limit型を定義
 export type LimitQuery = number
 
+// ダッシュボードからリクエストできるクエリの型 (bodyに載せる)
 export type PrismaQueryRequest<T extends keyof ModelMap> = {
   tableName: T
   query?: {
@@ -47,8 +47,12 @@ export type PrismaQueryRequest<T extends keyof ModelMap> = {
   skip?: number
 }
 
-// ここに追加していく
+// ここにダッシュボードに追加したいモデルを追加していく
 export type ModelMap = {
-  user: Model<p.User, 'User'>
-  // adminUser: Model<p.AdminUser, 'AdminUser'>
+  user: Model<prisma.User, 'User'>
+  adminUser: Model<prisma.AdminUser, 'AdminUser'>
+  stripeCheckout: Model<prisma.StripeCheckout, "StripeCheckout">
+  stripeSubscription: Model<prisma.StripeSubscription, "StripeSubscription">
+  contactHistory: Model<prisma.ContactHistory, "ContactHistory">
+
 }
