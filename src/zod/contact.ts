8b3f31c3bf1fb0280ff1244055f5c9e335b8fa02
Nick <naookiokd@gmail.com>
2024-10-10 18:52:40 +0900

    Next #248

--- src/zod/contact.ts
+++ src/zod/contact.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 import { toZodSchema } from "./common";
 import { sendContactParams } from "src/service/contact/sendContactParams"
 
-export const zodContactText = z.string().min(1).max(300, { message: "contactText" })
+export const zodContactText = z.string().min(1, { message: "stringRequired" }).max(300, { message: "contactText" })
 
 export const zodContactCreateSchema = z.object<toZodSchema<Omit<sendContactParams, "recaptchaToken">>>({
   email: z.string().email({ message: "zodEmailError" }),
