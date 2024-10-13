e3ddc28216738ee45ffc9c3a4a7a1d5f27d7ac51
Nick <naookiokd@gmail.com>
2024-10-13 20:12:04 +0900

    Node #186

--- src/service/admin/manage/passage/fetchSinglePassageParams.ts
+++ src/service/admin/manage/passage/fetchSinglePassageParams.ts
@@ -1,7 +1,3 @@
 import { PassageWithRelatedData } from "../../../passage/passageType"
 
-export type fetchSinglePassageParams = {
-  passageId: string
-}
-
 export type fetchSinglePassageResponse = PassageWithRelatedData
