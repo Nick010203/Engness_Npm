009d34d033f0d3b6be881963f7684c729e4ad84a
Nick <naookiokd@gmail.com>
2024-12-06 20:58:59 +0900

    Next #390

--- src/service/user/userDataForFront.ts
+++ src/service/user/userDataForFront.ts
@@ -3,7 +3,7 @@ import type { availablePlanList } from "../stripe/subscriptionPlanAndPriceIdMap"
 
 export const userColumnForFront = ['id', 'userName', 'email', 'gender', 'city', 'birthYear', "birthMonth"] as const
 export type userColumnForFront = (typeof userColumnForFront)[number]
-export type userDataForFront = Pick<User, userColumnForFront> & { subscribingPlan: availablePlanList }
+export type userDataForFront = Pick<User, userColumnForFront> & { subscribingPlan: availablePlanList & "Invited" }
 
 export const sessionColumnForFront = ["id", 'userName'] as const
 export type sessionColumnForFront = (typeof sessionColumnForFront)[number]
