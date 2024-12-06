34c0b56732395c0f9d529b28e9d72fdfd2ccebe5
Nick <naookiokd@gmail.com>
2024-12-06 19:28:29 +0900

    Node #221

--- src/service/stripe/subscriptionPlanAndPriceIdMap.ts
+++ src/service/stripe/subscriptionPlanAndPriceIdMap.ts
@@ -1,13 +1,4 @@
-type environment = "development" | "production"
+
+
 export type availablePlanList = "planE" | "planN"
 
-type map = {
-  [key in environment]: { [key: string]: availablePlanList }
-}
-
-export const subscriptionPlanAndPriceIdMap: map = {
-  development: {
-    price_1PB7gmKvZSHm7rkbGG9YKdQ7: 'planN'
-  },
-  production: {}
-}
