2c7bddd41dc92c1f882a1e13e9437ae5d19d8a08
Nick <naookiokd@gmail.com>
2024-12-07 14:01:40 +0900

    Node #223

--- src/service/user/userDataForFront.ts
+++ src/service/user/userDataForFront.ts
@@ -1,9 +1,9 @@
 import type { User } from "@prisma/client"
 import type { availablePlanList } from "../stripe/subscriptionPlanAndPriceIdMap"
 
-export const userColumnForFront = ['id', 'userName', 'email', 'gender', 'city', 'birthYear', "birthMonth"] as const
+export const userColumnForFront = ['id', 'userName', 'email', 'gender', 'city', 'birthYear', "birthMonth", "invited", "invitedUntil"] as const
 export type userColumnForFront = (typeof userColumnForFront)[number]
-export type userDataForFront = Pick<User, userColumnForFront> & { subscribingPlan: "Invited" | availablePlanList }
+export type userDataForFront = Pick<User, userColumnForFront> & { subscribingPlan: "Invited" | availablePlanList } & {}
 
 export const sessionColumnForFront = ["id", 'userName'] as const
 export type sessionColumnForFront = (typeof sessionColumnForFront)[number]
