e1daa57efa14728e3bba41baa62078eb6c7558fb
Nick <naookiokd@gmail.com>
2024-12-08 19:39:18 +0900

    Node #228

--- src/service/passage/fetchPassageListParams.ts
+++ src/service/passage/fetchPassageListParams.ts
@@ -1,11 +1,11 @@
-import type { Passage, EnglishLevel } from "@prisma/client"
+import type { Passage, LikePassage } from "@prisma/client"
 
 export type fetchPassageListParams = {
   englishLevelId: string | "all"
 }
 
 // passage.freeのカラムをサブスク有無によってpassage.challengeableに上書く
-export type PassageWithChallengeAbility = Passage & { LikePassage: { id: number } } & { challengeable: boolean }
+export type PassageWithChallengeAbility = Passage & { LikePassage: LikePassage } & { challengeable: boolean }
 
 // EnglishLevelがkeyになっている
 export type PassageListGroupedByEnglishLevel = { [key: string]: PassageWithChallengeAbility[] }[]
